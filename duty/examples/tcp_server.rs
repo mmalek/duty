4c9819fbc820a3ff5e5e1a140a88d39ef2df756c
Michał Małek <michalm@fastmail.fm>
2022-11-07 15:16:17 +0100

    Remove redundant struct

--- duty/examples/tcp_server.rs
+++ duty/examples/tcp_server.rs
@@ -8,16 +8,6 @@ use ttv_calc::TtvCalc;
 mod ttv_calculator;
 use ttv_calculator::Calculator;
 
-struct TtvCalcWorker {
-    factor: f64,
-}
-
-impl TtvCalc for TtvCalcWorker {
-    fn ttv_calc(&self, from: u64, to: u64) -> Vec<f64> {
-        (from..to).map(|x| x as f64 * self.factor).collect()
-    }
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind("0.0.0.0")?;
 
